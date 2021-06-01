615304d806b366427ebb779a1ce10d397a753c70
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-06-01 19:19:36 +0530

    Type 1

---
+++ test.ts
@@ -0,0 +1,17 @@
+let radius = 4;
+// assigning a string value to the radius will cause errors
+
+let coder: string = 'Devendra';
+//type-annotation use.
+
+//Dynamic Type annotation
+let anySpecifier: any = 'Anything of any data type can be assigned to this';
+
+// Uasge of Types - Complex
+
+// Fixing the type inside an array
+let names: string[] = ['Devendra', 'Mainsh', 'Steve', 'Steps'];
+let booltest: boolean[] = [true, false, true, true, false];
+
+let check: any[] = ['Dev', 'Manish', 'Krishna', 4, true];
+// specified the array data type applicable to all members
