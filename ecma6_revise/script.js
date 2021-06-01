8cde7b656538ca567314cd2f9c5601ba980d591b
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-06-01 18:18:14 +0530

    Classes

--- ecma6_revise/script.js
+++ ecma6_revise/script.js
@@ -14,7 +14,7 @@ var person = {
     }
 }
 
-person.printHobbies();
+// person.printHobbies();
 
 //Arrow function solving the "this" scope issue errors.
 var person_arrow = {
@@ -28,4 +28,51 @@ var person_arrow = {
     }
 }
 
-person_arrow.printHobbies();
+// person_arrow.printHobbies();
+
+//Destructing in ECMA6
+var [first, , last] = ['javascript', 'java', 'node.js'];
+// console.log(first);
+// console.log(last);
+
+// Generator Functions
+// Special types of function that pause on a particular statement and return the value
+
+function* myFunction(i) {
+    yield i;
+    yield i + 5;
+}
+
+var gen = myFunction(10);
+
+// console.log(gen.next());
+// console.log(gen.next());
+
+// Class - ES6
+class personClass {
+    constructor(name, age, profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+}
+
+class student extends personClass {
+    constructor(school, grade) {
+            super('Devendra', 20, 'Studying');
+
+            this.school = school;
+            this.grade = grade;
+        }
+        // getters and setters function
+
+    getAge() {
+        return this.age
+    }
+    setName(name) {
+        this.name = name;
+    }
+}
+
+let dev = new student('EE', 3);
+// console.log(dev);
