5d3d0c65064348270c905010bb85d733d7ea8c5f
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-06-01 18:44:17 +0530

    Promises Server Side Fetch

--- ecma6_revise/promise.js
+++ ecma6_revise/promise.js
@@ -28,4 +28,40 @@ myPromise
         // console.log("The promise was rejected!");
     })
 
-// Server Side Fetching
+// Server Side Fetching enabled with promises.
+
+function getData(method, url) {
+    return new Promise((resolve, reject) => {
+
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: this.statusText
+                });
+            }
+        };
+
+        xhr.onerror = function() {
+            reject({
+                status: this.status,
+                statusText: this.statusText
+            })
+        };
+
+        xhr.send();
+    })
+}
+
+getData('GET', 'https://jsonplaceholder.typicode.com/users')
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
