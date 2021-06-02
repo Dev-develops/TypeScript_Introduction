1ff43205594d4ff1dd18aa77f09a491a8bbf185c
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-06-02 14:01:51 +0530

    Constructor

--- constructor.js
+++ constructor.js
@@ -28,3 +28,22 @@ let myAutomaticClass = new classWithAutomaticProperties(100, 'ClassName');
 console.log(`id: ${myAutomaticClass.id}`);
 // console.log(`name: ${myAutomaticClass.name}`); causes error because name is private and only accessible inside class.
 // Readonly property is also available in the class similarly to the interface.
+class testClass {
+    static printData() {
+        console.log(this._hobby);
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+}
+testClass._hobby = 'Static Property'; // marked static
+let test = new testClass();
+test.name = 'Devendra';
+console.log(test.name); //private property accessed with getters and setters.
+testClass._hobby = 'Playing Video Games';
+// available without creating an instance of the class.
+// static functions
+testClass.printData();
--- constructor.ts
+++ constructor.ts
@@ -40,3 +40,31 @@ let myAutomaticClass = new classWithAutomaticProperties(100, 'ClassName');
 console.log(`id: ${myAutomaticClass.id}`);
 // console.log(`name: ${myAutomaticClass.name}`); causes error because name is private and only accessible inside class.
 // Readonly property is also available in the class similarly to the interface.
+
+class testClass {
+    private _id: number;
+    private _name: string;
+    static _hobby: string = 'Static Property'; // marked static
+
+    static printData(): void {
+        console.log(this._hobby);
+    }
+
+    get name(): string{
+        return this._name;
+    }
+    set name(value: string){
+        this._name = value;
+    }
+
+}
+
+let test = new testClass();
+test.name = 'Devendra';
+
+console.log(test.name); //private property accessed with getters and setters.
+
+testClass._hobby = 'Playing Video Games';
+// available without creating an instance of the class.
+// static functions
+testClass.printData();
